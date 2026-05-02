300bb207eff2e755a83b3994270ec6809b417061
mmaroof487 <mmaroof487gmail.com git config --global user.name mmaroof487git config --global user.email mmaroof487gmail.com>
2026-05-02 11:35:11 +0530

    chore(config): move secrets to env and configure project settings

---
+++ config.js
@@ -0,0 +1,11 @@
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+
+module.exports = {
+	email: "mm4116@srmist.edu.in",
+	name: "Mohammad Maroof",
+	rollNo: "RA2311003011703",
+	accessCode: process.env.ACCESS_CODE || "QkbpxH",
+	clientID: process.env.CLIENT_ID || "34c867a8-06fe-4a7e-af41-6465dee93039",
+	clientSecret: process.env.CLIENT_SECRET,
+	base: process.env.BASE_URL || "http://20.207.122.201/evaluation-service",
+};
